9344f79c3bac515ba49cade610619457732fb1a9
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-06-26 23:36:27 +0530

    Added : Employee Attendance

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -4,16 +4,15 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		
 		System.out.println(" ---- Welcome to Employee Wage Computation Program on Master Branch ---- ");
-		
+	
 		int is_present=1;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		
 		if(empCheck == is_present )
-			System.out.println(" Employee is Present ");
+			System.out.println("\n Employee is Present ");
 		else
-			System.out.println(" Employee is Absent ");
-		
-		
+			System.out.println("\n Employee is Absent ");
+
 	}
 	
 }
