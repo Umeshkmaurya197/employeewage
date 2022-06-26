4968ea87c1dbf8f8d95d7c6ad4a059920f700ae4
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-06-27 00:03:58 +0530

    Added: Part time employee feature

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -6,23 +6,35 @@ public class EmployeeWageBuilder {
 		
 		System.out.println(" ---- Welcome to Employee Wage Computation Program on Master Branch ---- ");
 	
-		 int is_present=1;
-		 int wagePerHour=20;
-		 int workingHours=8;
-		 double empCheck=Math.floor(Math.random()*10)%2;
+		int is_present=2;
+		int is_presentPartTime=1;
+		int wagePerHour=20;
+		int workingHours=8;
+		int partTimeHours=workingHours/2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		
-		if(empCheck == is_present )
-		{			
-			int empWage=wagePerHour*workingHours;
-			System.out.println(" Employee is Present ");
-		    System.out.println(" Employee Wage is : "+empWage);
+		
+		if(empCheck==is_present || empCheck==is_presentPartTime)
+		{ 
+			if(empCheck==is_present)
+			{
+				int empWage=wagePerHour*workingHours;
+				System.out.println("Employee is Present as full time");
+				System.out.println("Employee Wage is : "+empWage);
+			}
+			else
+			{
+				int empWage=wagePerHour*partTimeHours;
+				System.out.println("Employee is Present as Part time  ");
+				System.out.println("Employee Wage is : "+empWage);
+			}
 		}
 		else
-		{	
+		{
 			int empWage=0;
-			System.out.println(" Employee is Absent ");
-			System.out.println(" Employee Wage is : "+empWage);
-		}	
+			System.out.println("Enployee is Absent");
+			System.out.println("Employee Wage is :"+empWage);
+		}
 			
 
 	}
